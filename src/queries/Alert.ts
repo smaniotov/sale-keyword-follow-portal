9b173f72eb42ec4be421302fb1261461d32766fa
Vinícius Smanioto <smaniotov@gmail.com>
2020-01-27 02:24:00 -0300

    add new page query

--- src/queries/Alert.ts
+++ src/queries/Alert.ts
@@ -1,5 +1,25 @@
 import { gql } from 'apollo-boost';
 
+export const GET_ALERTS_PAGE = gql`
+    query getAlertsPage($keyword: String, $size: Float, $page: Float, $sort: Float){
+        getAlertsPage(
+            size: $size
+            page: $page
+            keyword: $keyword
+            sort: $sort
+        ) {
+            data {
+                _id
+                delay
+                keyword
+                sendTo
+                nextMessage
+            }
+            count
+        }
+    }
+`;
+
 export const GET_ALL_ALERTS = gql`
     {
         getAllAlerts {
@@ -12,6 +32,40 @@ export const GET_ALL_ALERTS = gql`
             createdAt
             updatedAt
         }
+    }
+`;
+
+export const CREATE_ALERT_INPUT = gql`
+    input CreateAlertInputType {
+        delay: Int!
+        keyword: String!
+        sendTo: String!
+    }
+`;
+
+export const CREATE_ALERT = gql`
+    mutation createAlert($alert: CreateAlertInputType!) {
+        createAlert(alert: $alert)
+    }
+`;
+
+export const UPDATE_ALERT = gql`
+    mutation updateAlert($id: String!, $alert: UpdateAlertInputType!) {
+        updateAlert(id: $id, alert: $alert)
+    }
+`;
+
+export const DELETE_ALERT = gql`
+    mutation deleteAlert($id: String!) {
+        deleteAlert(id: $id)
+    }
+`;
+
+export const UPDATE_ALERT_INPUT = gql`
+    input UpdateAlertInputType {
+        delay: Int
+        keyword: String
+        sendTo: String
     }
 `;
 
